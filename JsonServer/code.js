1858b3132b25fdca6426957d70402c81c19f85da
flexibrainsglobal <flexibrainsglobal@gmail.com>
2021-09-05 05:18:21 +0530

    Json Server

---
+++ JsonServer/code.js
@@ -0,0 +1,47 @@
+const faker = require('faker');
+const fs = require('fs');
+
+function generateData(){
+    let students = []
+    let books = []
+    let issuedbooks = []
+    for(let id=1; id<=20;id++){
+       
+       
+        students.push({
+            "roll_no":faker.datatype.number(),
+            "first_name":faker.name.firstName(),
+            "last_name":faker.name.lastName(),
+            "Class":faker.datatype.string(),
+            "division":faker.git.branch(),
+            "department":faker.commerce.department,
+            "dues":faker.commerce.price(),
+            "issued_books":0
+        });
+    }
+    for(let id=1; id<=15;id++){
+       
+        let issued_copies = faker.datatype.number()
+        let availble_copies = faker.datatype.number()
+
+        books.push({
+            "book_id":faker.datatype.number(),
+            "book_name":faker.system.fileName(),
+            "book_edition":faker.date.past(),
+            "book_department":faker.commerce.department(),
+            "available_copies":availble_copies,
+            "issued_copies":issued_copies,
+            "total_books":availble_copies+issued_copies,
+           
+        });
+    }
+    return {
+        "students":students,
+        "books":books,
+        "issuedbooks":issuedbooks
+    }
+}
+
+let dataObj = generateData();
+
+fs.writeFileSync('data.json',JSON.stringify(dataObj,null,'\t'));
