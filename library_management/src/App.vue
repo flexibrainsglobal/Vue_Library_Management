eedfb5581e8aa725bdb1f1c26a49ac776ddf3880
flexibrainsglobal <flexibrainsglobal@gmail.com>
2021-09-05 04:19:32 +0530

    Admin LTE 3 Added

--- library_management/src/App.vue
+++ library_management/src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Home/>
   </div>
-  <router-view/>
+ 
 </template>
-
+<script>
+import Home from "./views/Home.vue"
+import "admin-lte"
+export default {
+  components:{
+    Home
+  }
+  
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
